432ed74fe971914538616a9b4668ff89aaaabac3
yellowweak <b00902076@csie.ntu.edu.tw>
2021-11-21 16:54:37 +0900

    ABC228

--- ABC228/D(union-find).cpp
+++ ABC228/D(union-find).cpp
@@ -16,6 +16,12 @@ typedef vector<vector<ll>> grid;
 typedef pair<ll, ll> pll;
 int mod = 1000000007;
 // END LIB
+int find(vi &who, int root){
+    if(who[root]!=root){
+        who[root] = find(who, who[root]);
+    }
+    return who[root];
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -23,17 +29,33 @@ int main(){
 
     int N=1048576;
     vll A(N,-1);
+    vi who(N,-1);
     int Q; cin>>Q;
     rep(i,0,Q){
-        int type, x;
+        ll type, x;
         cin >> type >> x;
+        int xmn = x%N;
         if(type==1){
-            for(int i=x; A[i%N]!=-1; i++){
-                A[i%N] = x;
+            if(who[xmn]==-1){
+                int l = who[(xmn+N-1)%N];
+                int r = who[(xmn+1)%N];
+                if(l==-1&&r==-1)    who[xmn] = xmn;
+                else if (l!=-1&&r==-1)  who[l] = who[xmn] = xmn;
+                else if (l==-1&&r!=-1)  who[xmn] = find(who, r);
+                else who[l] = who[xmn] = find(who, r);
+                A[xmn] = x;
+            }
+            else{
+                int l = find(who, xmn);
+                int target = (l+1)%N;
+                int r = (target+1)%N;
+                if(who[r]==-1)  who[l] = who[target] = target;
+                else who[l] = who[target] = find(who, r);
+                A[target] = x;
             }
         }
         else {
-            cout << A[x%mod]<<endl;
+            cout << A[xmn] <<endl;
         }
     }
 
