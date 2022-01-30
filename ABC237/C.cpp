a7547f60489ebdf1c06e6bd422ca960e77e44d02
yellowweak <b00902076@csie.ntu.edu.tw>
2022-01-31 00:41:57 +0900

    ABC237

---
+++ ABC237/C.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+// BEGIN LIB
+template <typename T> istream &operator>>(istream &is, vector<T> &vec) { for (auto &v : vec) is >> v; return is; }
+#define print_grid(x) for(auto &r:x){for(auto &c:r)cout<<c<<" ";cout<<endl;}cout<<endl;
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef vector<vector<ll>> grid;
+typedef pair<ll, ll> pll;
+int mod = 1000000007;
+// END LIB
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    string S; cin>>S;
+    int l=0, r = sz(S)-1;
+    int lc=0, rc=0;
+    while(S[r]=='a'){r--;rc++;}
+    while(S[l]=='a'){l++;lc++;}
+    bool flag=true;
+    while(flag && l<r){
+        if(S[l]!=S[r])  flag=false;
+        else {l++; r--;}
+    }
+    cout << (flag&&rc>=lc? "Yes":"No");
+
+    return 0;
+}
---
+++ ABC237/D.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+// BEGIN LIB
+template <typename T> istream &operator>>(istream &is, vector<T> &vec) { for (auto &v : vec) is >> v; return is; }
+#define print_grid(x) for(auto &r:x){for(auto &c:r)cout<<c<<" ";cout<<endl;}cout<<endl;
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef vector<vector<ll>> grid;
+typedef pair<ll, ll> pll;
+int mod = 1000000007;
+// END LIB
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int N;
+    string S; cin>>N>>S;
+    deque<int> dq={N};
+    for(int i=N-1; i>=0; i--){
+        if(S[i]=='R')   dq.emplace_front(i);
+        else            dq.emplace_back(i);
+    }
+    for(auto x:dq)  cout << x << " ";
+
+    return 0;
+}
