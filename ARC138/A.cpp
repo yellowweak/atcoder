2b2246332ed54f08221ab38a881cbea4f1797169
yellowweak <b00902076@csie.ntu.edu.tw>
2022-04-09 23:05:56 +0900

    ARC138

---
+++ ARC138/A.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+// BEGIN LIB
+template <typename T> istream &operator>>(istream &is, vector<T> &vec) { for (auto &v : vec) is >> v; return is; }
+#define print_grid(x) for(auto &r:x){for(auto &c:r)cout<<c<<" ";cout<<endl;}cout<<endl;
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef vector<vector<ll>> grid;
+typedef pair<ll, ll> pll;
+int mod = 1000000007;
+// END LIB
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int N,K; cin>>N>>K;
+    vi A(N); cin>>A;
+    vector<pii> st = {{A[0], 0}};
+    for(int i=1; i<K; i++){
+        while(!st.empty() && A[i]<st.back().first)   st.pop_back();
+        if(st.empty())  st.push_back(make_pair(A[i], i));
+        else if(A[i]==st.back().first)   st.back() = make_pair(A[i], i);
+        else st.push_back(make_pair(A[i],i));
+    }
+    int ans = INT_MAX;
+    vi f;
+    unordered_map<int,int> um;
+    for(auto [v,i]:st){
+        f.push_back(v);
+        um[v]=i;
+    }
+    for(int i=K; i<N; i++){
+        if(A[i]<=f[0])    continue;
+        // int x = A[i];
+        auto it = upper_bound(all(st), make_pair(A[i]-1, INT_MAX));
+        it--;
+        // int pos = it-f.begin();
+        ans = min(ans, i-it->second);
+    }
+    cout << (ans==INT_MAX? -1:ans) << endl;
+    return 0;
+}
